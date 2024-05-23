708b5bfe658b891025d8bfde5d59e261691cbe23
Raphael Berquier <raphael.berquier@gmail.com>
2024-05-23 11:45:12 +0200

    change function name

--- file_parser.py
+++ file_parser.py
@@ -4,7 +4,7 @@ import os
 source_directory = "/home/raphael/Documents/GitHub/Markdown-To-HTML/Source"
 destination_file = "/home/raphael/Documents/GitHub/Markdown-To-HTML/files_parsed.txt"
 s = ""
-def parcourir_dossier(dossier, relative_path):
+def walf_folder(dossier, relative_path):
     global s
     for element in os.listdir(dossier):
         chemin = os.path.join(dossier, element)
@@ -13,9 +13,9 @@ def parcourir_dossier(dossier, relative_path):
                 print(relative_path+"/"+chemin.split('/')[-1])
                 s += relative_path+"/"+chemin.split('/')[-1]+"\n"
         elif os.path.isdir(chemin):
-            parcourir_dossier(chemin, os.path.join(relative_path, element))
+            walf_folder(chemin, os.path.join(relative_path, element))
 
-parcourir_dossier(source_directory, "")
+walf_folder(source_directory, "")
 with open(destination_file, 'w', encoding="utf-8") as file:
     file.write(s)
 
