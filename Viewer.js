ed5507e916847bbfaf95c5f5d2b25e373e9d5312
DeadMall0w <100719400+DeadMall0w@users.noreply.github.com>
2024-05-29 22:16:08 +0200

    Add tables

--- Viewer.js
+++ Viewer.js
@@ -11,7 +11,6 @@ function fetchMarkdownFiles(filename) {
         .then(markdownText => {
             // Convert Markdown to HTML
             const htmlOutput = formatMarkdownToHTML(markdownText, filename);
-            //console.log(htmlOutput);
             markdown_content = document.getElementById('markdownContent');
             
             // Display the HTML output
@@ -23,7 +22,6 @@ function fetchMarkdownFiles(filename) {
             document.title = filename.slice(0, -3);
 
             generateTableOfContents();
-
         })
         .catch(error => {
             console.error('There was a problem fetching the Markdown file:', filename, error);
@@ -31,10 +29,11 @@ function fetchMarkdownFiles(filename) {
 }
 
 let TOCList = [];//table of content header list
-function formatHeader(line){
-    i = 0;
-    while (line[i] == "#"){
-        i+=1;
+
+function formatHeader(line) {
+    let i = 0;
+    while (line[i] == "#") {
+        i += 1;
     }
     const headerText = line.slice(i).trim();
     const headerId = headerText.toLowerCase().replace(/\s+/g, '-'); // Generate an id for the header
@@ -42,12 +41,75 @@ function formatHeader(line){
     return `<h${i} id="${headerId}">${headerText}</h${i}>`;
 }
 
+function convertAllMarkdownTableToHTML(text) {
+    let convertedText = '';
+    let tableBlock = '';
+    const lines = text.split('\n');
+    let isTableBlock = false;
 
-function formatMarkdownToHTML(markdownText, title){
-    htmlText = `<h1 id="Title">${title.slice(0, -3)}</h1><br>`;
-    htmlText += detectMarkdownExpression(markdownText);
+    lines.forEach(line => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith('|')) {
+            if (!isTableBlock) { // Beginning of a table block
+                isTableBlock = true;
+            }
+            tableBlock += trimmedLine + '\n';
+        } else {
+            if (isTableBlock) { // End of table block
+                isTableBlock = false;
+                convertedText += convertMarkdownTableToHTML(tableBlock);
+                tableBlock = '';
+            }
+            convertedText += line + '\n';
+        }
+    });
+
+    if (isTableBlock) { // If there's a table block at the end of the text
+        convertedText += convertMarkdownTableToHTML(tableBlock);
+    }
+
+    return convertedText;
+}
+
+function convertMarkdownTableToHTML(markdown) {
+    const lines = markdown.split('\n').filter(line => line.trim() !== '');
+    let html = '<table>';
+    let headerProcessed = false;
+
+    lines.forEach(line => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith('|')) {
+            const cells = trimmedLine.slice(1, -1).split('|').map(cell => cell.trim());
+            if (!headerProcessed) {
+                html += '<thead><tr>';
+                cells.forEach(header => {
+                    html += `<th>${header}</th>`;
+                });
+                html += '</tr></thead><tbody>';
+                headerProcessed = true;
+            } else {
+                if (cells.every(cell => cell.match(/^-+$/))) {
+                    return; // Skip the separator line
+                }
+                html += '<tr>';
+                cells.forEach(cell => {
+                    html += `<td>${cell}</td>`;
+                });
+                html += '</tr>';
+            }
+        }
+    });
+
+    html += '</tbody></table>';
+    return html;
+}
+
+function formatMarkdownToHTML(markdownText, title) {
+    let htmlText = `<h1 id="Title">${title.slice(0, -3)}</h1>`;
+    htmlText += convertAllMarkdownTableToHTML(markdownText);
+    htmlText = detectMarkdownExpression(htmlText);
     htmlText = formatSpace(htmlText);
-    //htmlText +=
+    console.log(htmlText);
     return htmlText;
 }
 
@@ -70,12 +132,11 @@ function loadNotePaths() {
                     notePaths[noteName] = line.trim();
                 }
             });
-            console.log(notePaths); // Debugging purpose
         })
         .catch(error => console.error('Error loading note paths:', error));
 }
 
-function detectMarkdownExpression(markdownText){
+function detectMarkdownExpression(markdownText) {
     return markdownText
         .replace(/\n/g, '<br>') // Replace line breaks with <br> tags
         // You can add more basic Markdown-to-HTML conversion rules here as needed
@@ -93,68 +154,59 @@ function detectMarkdownExpression(markdownText){
             } else {
                 return `<a href="#" class="notFound">${p1.trim()}</a>`;
             }
-        }); // Convert internal links
+        });
 }
 
-
-function closeParagraph(index){
-    if (index == 0){
+function closeParagraph(index) {
+    if (index == 0) {
         return "";
     }else if (index == 1) {
         return "</p>";
-    } else if (index == 2){
+    } else if (index == 2) {
         return "</ul>";
     }
 }
+
 // Function to format space, create <p> for text, <h1> for title, ect...
 function formatSpace(text) {
     const lines = text.split('<br>');
-    
-    let htmlOutput = '';
 
+    let htmlOutput = '';
     let currentSection = 0; // Variable pour suivre le dernier type de ligne traitée (0: autre, 1: paragraphe, 2:list, 3:code bloc)
 
     for (const line of lines) {
-        if (currentSection == 3){
-            if (line.startsWith('```')){
-                currentSection = 0; //* end of code block
-                console.log("End of code block: " + line)
-                htmlOutput += `</pre></code>`
-            }else{
-                htmlOutput += line+"<br>";
-                console.log(line+"<br>");
+        if (currentSection == 3) {
+            if (line.startsWith('```')) {
+                currentSection = 0; // end of code block
+                htmlOutput += `</code></pre>`;
+            } else {
+                htmlOutput += line + "<br>";
             }
-        }
-        else if (line.startsWith('#')) { //* Title 
-            htmlOutput += closeParagraph(currentSection);  //* Close section according to last section
-            htmlOutput += formatHeader(line); //* Create header lvl
-            currentSection = 0; //* Set current section to title
-
-        }else if(line.startsWith('```')){
-            console.log(line);
-            currentSection = 3; //* Set current section to code bloc
+        } else if (line.startsWith('#')) {
+            htmlOutput += closeParagraph(currentSection); // Close section according to last section
+            htmlOutput += formatHeader(line); // Create header lvl
+            currentSection = 0; // Set current section to title
+        } else if (line.startsWith('```')) {
+            currentSection = 3; // Set current section to code block
             htmlOutput += `<pre><code class="language-${line.slice(3)}">`;
-
-
-        }else if (line.startsWith('- ')) {
-            if (currentSection == 2) { //* It's already a list section
-                htmlOutput += `<li>${line.slice(2)}</li>`;//* Add the line and add <br>
-            } else { //* Create a new text section
-                htmlOutput += closeParagraph(currentSection);  //* Close section according to last section
+        } else if (line.startsWith('- ')) {
+            if (currentSection == 2) { // It's already a list section
+                htmlOutput += `<li>${line.slice(2)}</li>`; // Add the line and add <br>
+            } else { // Create a new text section
+                htmlOutput += closeParagraph(currentSection); // Close section according to last section
                 htmlOutput += `<ul><li>${line.slice(2)}</li>`;
             }
-            currentSection = 2; //* Set the current section to text 
-
-        } else if (line.trim() !== '') { //* It's a <p>
-            if (currentSection == 1) { //* It's already a text section
-                htmlOutput += `${line}<br>`;//* Add the line and add <br>
-            } else { //* Create a new text section
-                htmlOutput += closeParagraph(currentSection);  //* Close section according to last section
+            currentSection = 2; // Set the current section to text
+        } else if (line.trim() !== '') { // It's a <p>
+            if (currentSection == 1) { // It's already a text section
+                htmlOutput += `${line}<br>`; // Add the line and add <br>
+            } else { // Create a new text section
+                htmlOutput += closeParagraph(currentSection); // Close section according to last section
                 htmlOutput += `<p>${line}`;
             }
-            currentSection = 1; //* Set the current section to text
+            currentSection = 1; // Set the current section to text
         } else if (line.trim() == '') {
-            htmlOutput += closeParagraph(currentSection);  //* Close section according to last section
+            htmlOutput += closeParagraph(currentSection); // Close section according to last section
             currentSection = 0;
         } else {
             htmlOutput += line;
@@ -169,7 +221,6 @@ function formatSpace(text) {
 
 // Initial load
 loadNotePaths();
-// fetchMarkdownFiles('Hello World !.md'); // Load a default Markdown file
 
 // Explorer section
 document.addEventListener('DOMContentLoaded', () => {
@@ -246,8 +297,8 @@ function renderFileTree(tree, parentElement, currentPath = '') {
     });
 }
 
-//Table of contents
-function generateTableOfContents(content) {
+// Table of contents
+function generateTableOfContents() {
     const tocContainer = document.getElementById('tableOfContents'); // Assurez-vous d'avoir un conteneur avec cet ID dans votre HTML
     let tocHTML = '<h2>Table of Contents</h2><ul>';
 
