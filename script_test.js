3ee0ee1723c2593013b1e1440de1222a12831a44
DeadMall0w <100719400+DeadMall0w@users.noreply.github.com>
2024-05-29 23:21:04 +0200

    try things for resizing

---
+++ script_test.js
@@ -0,0 +1,44 @@
+
+var resizerRight = document.querySelector(".resizer-right"),
+   sidebarRight = document.querySelector(".sidebar-right");
+var resizerLeft = document.querySelector(".resizer-left"),
+   sidebarLeft = document.querySelector(".sidebar-left");
+
+function initResizerFn( resizerRight, sidebarRight, resizerLeft, sidebarLeft ) {
+
+   // track current mouse position in x var
+   var x, w;
+
+   function rs_mousedownHandler( e ) {
+
+      x = e.clientX;
+
+      var sbWidthRight = window.getComputedStyle( sidebarRight ).width;
+      w = parseInt( sbWidthRight, 10 );
+
+      document.addEventListener("mousemove", rs_mousemoveHandler);
+      document.addEventListener("mouseup", rs_mouseupHandler);
+   }
+
+   function rs_mousemoveHandler( e ) {
+      var dx = e.clientX - x;
+
+      var cw = w + dx; // complete width
+      
+      if ( cw < 700 ) {
+         sidebarRight.style.width = `${ cw }px`;
+      }
+   }
+   
+
+   function rs_mouseupHandler() {
+      // remove event mousemove && mouseup
+      document.removeEventListener("mouseup", rs_mouseupHandler);
+      document.removeEventListener("mousemove", rs_mousemoveHandler);
+   }
+
+   resizerRight.addEventListener("mousedown", rs_mousedownHandler);
+}
+
+
+initResizerFn( resizerRight, sidebarRight,resizerLeft, sidebarLeft );
